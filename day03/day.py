89d7936e5f256e173b384c0cbe321d52706c9e0a
Lukas Raffelt <lukas.raffelt@daimler.com>
2021-12-02 09:40:27 +0100

    prepare day3

---
+++ day03/day.py
@@ -0,0 +1,32 @@
+import helper
+
+def alg1(lines, printDebug):
+    return 0
+
+def alg2(lines, printDebug):
+    return 0
+
+
+def part1(fname: str, printDebug = False):
+    print("=== PART 1 ===")
+    print("-- {} --".format(fname))
+    result = 0
+    result = alg1(helper.input_as_lines(fname), printDebug)
+    print("Result = {}".format(result))
+    print()
+
+def part2(fname: str, printDebug = False):
+    print("=== PART 2 ===")
+    print("-- {} --".format(fname))
+    result = 0
+    result = alg2(helper.input_as_lines(fname), printDebug)
+    print("Result = {}".format(result))
+    print()
+    
+
+if __name__ == '__main__':
+    print("\n")
+    part1("day03/test.txt", True)
+    part1("day03/input.txt")
+    part2("day03/test.txt", True)
+    part2("day03/input.txt")
