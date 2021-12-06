cfd4cda9623237f79be3e0a65d56c819da823089
Lukas Raffelt <lukas.raffelt@googlemail.com>
2021-12-06 08:23:31 +0100

    cleanup

--- day06/day.py
+++ day06/day.py
@@ -32,28 +32,7 @@ def alg1(data, days, printDebug):
 
 
 def alg2(data, days, printDebug):
-    start = data
-    if printDebug: print(start)
-    if printDebug: print(Counter(start))
-    histogram = deque(maxlen=BIRTH+1)
-    for _ in range(max(REPRODUCTION+1, BIRTH+1)):
-        histogram.append(0)
-    if printDebug: print(histogram)
-    # initial Filling
-    for k, v in Counter(start).items():
-        histogram[k] = v
-    if printDebug: print("Init{:>3} ".format(0), end='')
-    if printDebug: print(histogram)
-    for day in range(days):
-        if printDebug: print("Day {:>3} ".format(day + 1), end='')
-        # pop and shift left
-        zeroes = histogram.popleft()
-        # birth some new fishes at last pos (should be 8)
-        histogram.append(zeroes)
-        histogram[REPRODUCTION] += zeroes
-        if printDebug: print(histogram)
-    print(sum(histogram))
-    return sum(histogram)
+    return alg1(data, days, printDebug)
 
 
 def part1(fname: str, days, printDebug = False):
@@ -79,7 +58,7 @@ if __name__ == '__main__':
     print(test_fname)
     print("--- Day 6: Lanternfish ---\n")
     part1(test_fname, 18, True)
-    part1(test_fname, 80, True)
+    part1(test_fname, 80, False)
     part1(input_fname, 80)
-    part2(test_fname, 256, True)
+    part2(test_fname, 256, False)
     part2(input_fname, 256)
