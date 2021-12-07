8320772c889a7279f342ad1ec1e5c776a6dbaa4b
Lukas Raffelt <lukas.raffelt@daimler.com>
2021-12-07 08:24:56 +0100

    day07

--- day07/day.py
+++ day07/day.py
@@ -3,19 +3,43 @@ import os
 import helper
 
 
-def alg1(lines, printDebug):
-    return 0
+def alg1(data, printDebug):
+    MIN = min(data)
+    MAX = max(data)
+    if printDebug: print("min={}, max={}, data={}".format(MIN,MAX,data))
+    costs = []
+    min_cost = 99999999999999
+    for i in range(MIN, MAX+1):
+        if printDebug: print("Pos = {} ".format(i), end='')
+
+        cost = sum(map(lambda n: abs(n-i),data))
+        min_cost = min(min_cost, cost)
+        if printDebug: print(cost)
+        costs.append((i, cost))
+    return min_cost
 
 
 def alg2(data, printDebug):
-    return 0
+    MIN = min(data)
+    MAX = max(data)
+    if printDebug: print("min={}, max={}, data={}".format(MIN,MAX,data))
+    costs = []
+    min_cost = 99999999999999
+    for i in range(MIN, MAX+1):
+        if printDebug: print("Pos = {} ".format(i), end='')
+
+        cost = sum(map(lambda n: int(abs(n-i)*(abs(n-i)+1)/2),data))
+        min_cost = min(min_cost, cost)
+        if printDebug: print(cost)
+        costs.append((i, cost))
+    return min_cost
 
 
 def part1(fname: str, printDebug = False):
     print("=== PART 1 ===")
     print("-- {} --".format(fname))
     result = 0
-    result = alg1(helper.input_as_lines(fname), printDebug)
+    result = alg1(helper.single_line_as_ints(fname), printDebug)
     print("Result = {}".format(result))
     print()
 
@@ -23,7 +47,7 @@ def part2(fname: str, printDebug = False):
     print("=== PART 2 ===")
     print("-- {} --".format(fname))
     result = 0
-    result = alg2(helper.input_as_lines(fname), printDebug)
+    result = alg2(helper.single_line_as_ints(fname), printDebug)
     print("Result = {}".format(result))
     print()
     
@@ -32,7 +56,7 @@ if __name__ == '__main__':
     test_fname = os.path.join(os.path.dirname(__file__), 'test.txt')
     input_fname = os.path.join(os.path.dirname(__file__), 'input.txt')
 
-    print("\n")
+    print("--- Day 7: The Treachery of Whales ---\n")
     part1(test_fname, True)
     part1(input_fname)
     part2(test_fname, True)
--- day07/test_day.py
+++ day07/test_day.py
@@ -1,8 +1,10 @@
 import os
 import unittest
 
-import day07.day as day
-import day07.helper as helper
+# import day07.day as day
+# import day07.helper as helper
+import day
+import helper 
 
 
 class TestDay(unittest.TestCase):
@@ -10,23 +12,23 @@ class TestDay(unittest.TestCase):
     input_fname = os.path.join(os.path.dirname(__file__), 'input.txt')
 
     def test_part_1a(self):
-        expected = 0
-        result = day.alg1(helper.input_as_lines(self.test_fname), False)
+        expected = 37
+        result = day.alg1(helper.single_line_as_ints(self.test_fname), False)
         self.assertEqual(expected, result)
     
     def test_part_1b(self):
-        expected = 0
-        result = day.alg1(helper.input_as_lines(self.input_fname), False)
+        expected = 349769
+        result = day.alg1(helper.single_line_as_ints(self.input_fname), False)
         self.assertEqual(expected, result)
     
     def test_part_2a(self):
-        expected = 0
-        result = day.alg2(helper.input_as_lines(self.test_fname), False)
+        expected = 168
+        result = day.alg2(helper.single_line_as_ints(self.test_fname), False)
         self.assertEqual(expected, result)
 
     def test_part_2b(self):
-        expected = 0
-        result = day.alg2(helper.input_as_lines(self.input_fname), False)
+        expected = 99540554
+        result = day.alg2(helper.single_line_as_ints(self.input_fname), False)
         self.assertEqual(expected, result)
 
 
