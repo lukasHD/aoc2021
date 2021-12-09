ddaa61e600329deebc642c902aebdb895e82c946
Lukas Raffelt <lukas.raffelt@googlemail.com>
2021-12-09 07:45:10 +0100

    day09

--- day09/day.py
+++ day09/day.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 from dataclasses import dataclass
 
 import helper
@@ -11,7 +12,10 @@ class Point:
     y: int = 0
 
     def __str__(self) -> str:
-        return "[{},\t{}]".format(self.x, self.y)
+        return "[{:>3},{:>3}]".format(self.x, self.y)
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
 
 def parse_line(data):
     formated = []
@@ -33,6 +37,33 @@ def get_straight_neighbours(data, point: Point):
             continue
     return neighbours
 
+def get_min_lower_neighbor_point(data, point :Point):
+    minNeighbor = Point()
+    minValue = 10
+    pointValue = data[point.x][point.y]
+    for delta in [Point(1,0), Point(-1,0), Point(0,1), Point(0,-1)]:
+        try:
+            px = point.x + delta.x
+            py = point.y + delta.y
+            if px < 0 or py < 0:
+                continue
+            if data[px][py] < minValue:
+                minNeighbor = Point(px,py)
+                minValue = data[px][py]
+        except IndexError:
+            continue
+    # print("minValue={}, pointValue={}".format(minValue, pointValue))
+    if minValue < pointValue:
+        return minNeighbor
+    return None
+
+def flow_down(data, point: Point):
+    print(point, end=" ")
+    next_point = get_min_lower_neighbor_point(data, point)
+    if next_point == None:
+        return point
+    return flow_down(data, next_point)
+
 def pretty(data):
     for line in data:
         for element in line:
@@ -42,7 +73,7 @@ def pretty(data):
 def alg1(data, printDebug):
     result = 0
     floormap = parse_line(data=data)
-    pretty(floormap)
+    if printDebug: pretty(floormap)
     for x, line in enumerate(floormap):
         for y, el in enumerate(line):
             neighbours = get_straight_neighbours(floormap, Point(x,y))
@@ -54,7 +85,27 @@ def alg1(data, printDebug):
 
 
 def alg2(data, printDebug):
-    return 0
+    """FLow down until we are in a minimum and then add point to size of bassin
+    Locations of height 9 do not count as being in any basin, and all other locations will always be part of exactly one basin.
+    """
+    basins = defaultdict(int)
+    floormap = parse_line(data=data)
+    if printDebug: pretty(floormap)
+    for x, line in enumerate(floormap):
+        for y, el in enumerate(line):
+            if el == 9:
+                continue
+            basin_point = flow_down(floormap, Point(x,y))
+            print("{} --> {}".format(Point(x,y), basin_point))
+            basins[basin_point] += 1
+
+    result = 1
+    basin_sizes = basins.values()
+    print(basin_sizes)
+    for n in list(sorted(basin_sizes))[-3:]:
+        print(n)
+        result *= n
+    return result
 
 
 def part1(fname: str, printDebug = False):
@@ -79,7 +130,7 @@ if __name__ == '__main__':
     input_fname = os.path.join(os.path.dirname(__file__), 'input.txt')
 
     print("--- Day 9: Smoke Basin ---\n")
-    part1(test_fname, True)
-    part1(input_fname)
-    # part2(test_fname, True)
-    # part2(input_fname)
+    # part1(test_fname, True)
+    # part1(input_fname)
+    part2(test_fname, True)
+    part2(input_fname)
