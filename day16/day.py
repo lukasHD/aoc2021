b6106a8882cd08422086002e70693069d6de1993
Lukas Raffelt <lukas.raffelt@googlemail.com>
2021-12-17 22:24:34 +0100

    more

--- day16/day.py
+++ day16/day.py
@@ -3,12 +3,98 @@
 """
 
 import os
+from copy import copy
 
 import helper
 
 
+def parse_bits(data, version_sum = 0):
+    # Every packet begins with a standard header:
+    # the first three bits encode the packet version,
+    # and the next three bits encode the packet type ID. 
+    # These two values are numbers;
+    # all numbers encoded in any packet are represented as binary with the most significant bit first.
+    # For example, a version encoded as the binary sequence 100 represents the number 4.
+    version = data[0:3]
+    id = data[3:6]
+    print(f"version={version} id={id}")
+    version = int(version, 2)
+    id = int(id, 2)
+    print(f"version={version} id={id}")
+    version_sum += version
+    rest = data[6:]
+    # Packets with type ID 4 represent a literal value. 
+    # Literal value packets encode a single binary number. 
+    # To do this, the binary number is padded with leading zeroes until its length is a multiple of four bits, 
+    # and then it is broken into groups of four bits. 
+    # Each group is prefixed by a 1 bit except the last group, which is prefixed by a 0 bit. 
+    # These groups of five bits immediately follow the packet header.
+    if id == 4:
+        # literal value
+        full_value = ""
+        while True:
+            seq = rest[0] 
+            value = rest[1:5]
+            rest = rest[5:]
+            print(f"seq={seq} value={value} rest={rest}")
+            full_value = full_value + value
+            print(f"full_value={full_value}")
+            if seq == "0":
+                break
+        full_value = int(full_value, 2)
+        print(f"full_value={full_value}, rest={rest}")
+        return str(full_value), rest, version_sum
+    # Every other type of packet (any packet with a type ID other than 4) represent an operator 
+    # that performs some calculation on one or more sub-packets contained within. 
+    length_type = rest[0]
+    rest = rest[1:]
+    print(f"length_type={length_type}")
+    # If the length type ID is 0, then the next 15 bits are a number that represents the total length in bits of the sub-packets contained by this packet.
+    # If the length type ID is 1, then the next 11 bits are a number that represents the number of sub-packets immediately contained by this packet.
+    if length_type == "0":
+        length = rest[0:15]
+        print(f"length={length}")
+        length = int(length, 2)
+        rest = rest[15:]
+        print(f"length={length} rest={rest}")
+        sub_packages = rest[:length]
+        rest = rest[length:]
+        print(f"sub_packages={sub_packages}")
+        
+        # parse all subpackages 
+        subs = []
+        value, other_subpackages, version_sum = parse_bits(sub_packages, version_sum)
+        subs.append(str(value))
+        while len(other_subpackages) != 0 and int(other_subpackages, 2) != 0:
+            sub_packages = copy(other_subpackages)
+            value, other_subpackages, version_sum = parse_bits(sub_packages, version_sum)
+            subs.append(str(value))
+        print(f"subs={subs}, rest={rest}")
+        return subs, rest, version_sum
+    if length_type == "1":
+        number = rest[0:11]
+        print(f"number={number}")
+        number = int(number, 2)
+        rest = rest[11:]
+        print(f"number={number} rest={rest}")
+        # parse all those packages
+        subs = []
+        for _ in range(number):
+            value, rest, version_sum = parse_bits(rest, version_sum)
+            subs.append(str(value))
+        print(f"subs={subs}, rest={rest}")
+        return subs, rest, version_sum
+
+
+
+
+
 def alg1(data, print_debug):
-    return 0
+    for line in data:
+        bits = (bin(int(line, 16))[2:]).zfill(len(line) * 4)
+        print(f"\n\n{bits}")
+        _, _ , result = parse_bits(bits)
+    return result
 
 
 def alg2(data, print_debug):
@@ -36,8 +122,8 @@ if __name__ == '__main__':
     test_fname = os.path.join(os.path.dirname(__file__), 'test.txt')
     input_fname = os.path.join(os.path.dirname(__file__), 'input.txt')
 
-    print("\n")
+    print("--- Day 16: Packet Decoder ---\n")
     part1(test_fname, True)
-    part1(input_fname)
-    part2(test_fname, True)
-    part2(input_fname)
+    # part1(input_fname)
+    # part2(test_fname, True)
+    # part2(input_fname)
