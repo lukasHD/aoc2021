262eb89ba3881f47b5a762cc64f985caef318823
Lukas Raffelt <lukas.raffelt@daimler.com>
2021-12-20 08:15:26 +0100

    prepare rest of the dazs

---
+++ day24/day.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Example Google style docstrings.
+"""
+
+import os
+
+import helper
+
+
+def alg1(data, print_debug):
+    return 0
+
+
+def alg2(data, print_debug):
+    return 0
+
+
+def part1(fname: str, print_debug = False):
+    print("=== PART 1 ===")
+    print(f"-- {fname} --")
+    result = 0
+    result = alg1(helper.input_as_lines(fname), print_debug)
+    print(f"Result = {result}")
+    print()
+
+def part2(fname: str, print_debug = False):
+    print("=== PART 2 ===")
+    print(f"-- {fname} --")
+    result = 0
+    result = alg2(helper.input_as_lines(fname), print_debug)
+    print(f"Result = {result}")
+    print()
+
+
+if __name__ == '__main__':
+    test_fname = os.path.join(os.path.dirname(__file__), 'test.txt')
+    input_fname = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+    print("\n")
+    part1(test_fname, True)
+    part1(input_fname)
+    part2(test_fname, True)
+    part2(input_fname)
